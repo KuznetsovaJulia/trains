e5a220049825c0849a222fc0565385b6f127a9da
Vladik <vlad1997kov@gmail.com>
2018-01-08 00:17:05 +0300

    route skeleton

--- route.rb
+++ route.rb
@@ -5,20 +5,44 @@
 # Может выводить список всех станций по-порядку от начальной до конечной
 
 class Route
-  attr_accessor :route, :end_station,
-                :starting_station, :list_station
-  attr_reader :station
+  attr_accessor :station_list
 
   def initialize(station_list)
     @station_list = station_list
   end
 
   def add_station(station)
-    self.route = @route + [station.name]
+    return 'Station is invalid' unless station_is_valid(station)
+
+    if station_is_present_in_list(station)
+      station_list << station
+    else
+      'Station already exists'
+    end
   end
 
-  def remove_station(number)
-    self.route = @route - [route[number - 1]]
+  def remove_station(station)
+    station_list.delete_if {|st| st.name == station.name }
+  end
+
+  def first_station
+    station_list.first
+  end
+
+  def last_station
+    station_list.last
+  end
+
+  private
+
+  def station_is_valid(station)
+    station.is_a?(RailwayStation)
+  end
+
+
+
+  def station_is_present_in_list(station)
+    #Узнать, есть ли станция в списке как ?))
   end
 end
 
